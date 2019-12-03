fb15276bd5b2c8b48f0cd2cdb44a4a10845b0065
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-03 12:20:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-let totalBatteries = batteryBatches.reduce(function(grandTota,battery){
-return grandTotal + battery;},0);
+let totalBatteries = batteryBatches.reduce(function(grandTotal,battery){
+  return grandTotal + battery;},0);
